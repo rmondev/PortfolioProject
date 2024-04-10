f86b6a08232d04c9cf3462bf608d85ec0e3bd0be
Riccardo Moncada <rmondev@gmail.com>
2024-04-09 21:42:35 -0400

    added animated link listing to responsive menu

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 const Homepage = () => {
   return (
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+
+
       {/* IMAGE CONTAINER */}      
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image
@@ -12,13 +14,13 @@ const Homepage = () => {
         fill
         className='object-contain'
         />
-
-        
       </div>
+
+
       {/* TEXT CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
         {/* TITLE */}
-        <h1 className=" text-4xl font-bold md:text-6xl">Crafting Digital Experiences, Designing Tomorrow</h1>
+        <h1 className="text-4xl font-bold md:text-6xl">Crafting Digital Experiences, Designing Tomorrow</h1>
         
         {/*DESCRIPTION */}
         <p className="md:text-xl">Welcome to my digital canvas,  where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
