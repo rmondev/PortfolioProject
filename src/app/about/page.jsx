145f674ff075bea30be270ab39a39b16db208c79
Riccardo Moncada <rmondev@gmail.com>
2024-04-25 12:12:27 -0400

    added work experience section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -13,7 +13,7 @@ const AboutPage = () => {
       transition={{duration: 1}}
       >
       {/*CONTAINER*/}
-      <div className=''>
+      <div className='h-full overflow-scroll lg:flex'>
         {/*TEXT CONTAINER*/}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
           {/*BIOGRAPHY CONTAINER*/}
@@ -110,9 +110,97 @@ const AboutPage = () => {
             ></path>
             </svg>
           </div>
+
+
           {/*EXPERIENCE CONTAINER*/}
-          <div className="">
-            EXPERIENCE
+          <div className="flex flex-col gap-12 justify-center pb-48">
+          {/*EXPERIENCE TITLE*/}
+          <h1 className='font-bold text-2xl'>EXPERIENCE</h1>
+
+            {/*EXPERIENCE LIST*/}
+            <div className="">
+
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                  <div className="w-1/3">
+                    {/* JOB TITLE */}
+                    <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">Programmer Analyst</div>
+                    {/* JOB DESCRIPTION */}
+                    <div className="p-3 text-sm italic">The role focused on enhancing and automating internal systems to optimize functionality and efficiency.</div>
+                    {/* JOB DATE */}
+                    <div className="p-3 text-red-400 text-sm font-semibold">2022 - 2023</div>
+                    {/* JOB COMPANY */}
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">Toronto Transit Commission</div>
+                  </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                  <div className="w-1/3">
+
+                  </div>
+              </div>
+               
+            {/* EXPERIENCE LIST ITEM */}
+            <div className="flex justify-between h-48">
+            {/* LEFT */}
+              <div className="w-1/3">
+              </div>
+            {/* CENTER */}
+            <div className="w-1/6 flex justify-center">
+              {/* LINE */}
+              <div className="w-1 h-full bg-gray-600 rounded relative">
+                {/* LINE CIRCLE */}
+                <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+              </div>
+            </div>
+            {/* RIGHT */}
+              <div className="w-1/3">
+                {/* JOB TITLE */}
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">Programmer Analyst</div>
+                {/* JOB DESCRIPTION */}
+                <div className="p-3 text-sm italic">The role focused on enhancing and automating internal systems to optimize functionality and efficiency.</div>
+                {/* JOB DATE */}
+                <div className="p-3 text-red-400 text-sm font-semibold">2022 - 2023</div>
+                {/* JOB COMPANY */}
+                <div className="p-1 rounded bg-white text-sm font-semibold w-fit">Toronto Transit Commission</div>
+              </div>
+            </div>
+
+            {/* EXPERIENCE LIST ITEM */}
+            <div className="flex justify-between h-48">
+                {/* LEFT */}
+                  <div className="w-1/3">
+                    {/* JOB TITLE */}
+                    <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">Programmer Analyst</div>
+                    {/* JOB DESCRIPTION */}
+                    <div className="p-3 text-sm italic">The role focused on enhancing and automating internal systems to optimize functionality and efficiency.</div>
+                    {/* JOB DATE */}
+                    <div className="p-3 text-red-400 text-sm font-semibold">2022 - 2023</div>
+                    {/* JOB COMPANY */}
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">Toronto Transit Commission</div>
+                  </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                  <div className="w-1/3">
+
+                  </div>
+              </div>
+
+            </div>
           </div>
         </div>
         
