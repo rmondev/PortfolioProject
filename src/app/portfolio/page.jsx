7880ab6f4ba7f34fbc775f2e2ce063fa7e327818
Riccardo Moncada <rmondev@gmail.com>
2024-05-01 17:51:29 -0400

    fix text size

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -90,7 +90,7 @@ const PortfolioPage = () => {
         </div>
       </div>
       <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
-        <h1 className="text-8xl">Do you have a project?</h1>
+        <h1 className="text-6xl sm:text-6xl md:text-6xl lg:text-8xl xl:text-8xl">Do you have a project?</h1>
         <div className="relative">
           <motion.svg
             animate={{ rotate: 360 }}
