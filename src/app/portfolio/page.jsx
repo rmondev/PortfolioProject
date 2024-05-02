40eee0691e9df5b02f5c21089fa232fbadde1d1f
Riccardo Moncada <rmondev@gmail.com>
2024-05-02 13:16:06 -0400

    added met gallery app

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -26,7 +26,7 @@ const PortfolioPage = () => {
     },
     {
       id: 3,
-      color: "from-red-300 to-blue-300",
+      color: "from-violet-300 to-red-300",
       title: "iOS Tax Filing Management App",
       desc: "This is an app I created to Tax Filing Management App to handle a customer database for a tax filing agency. This application implements iOS CoreData to enable database persistence.",
       img: ["/screenshots/iOSTaxFilingApp/MainScreen.png","/screenshots/iOSTaxFilingApp/BeforeScreen.png","/screenshots/iOSTaxFilingApp/AfterScreen.png",],
@@ -34,7 +34,7 @@ const PortfolioPage = () => {
     },
     {
       id: 4,
-      color: "from-violet-300 to-purple-300",
+      color: "from-red-300 to-blue-300",
       title: "Android To-Do List App",
       desc: "This application showcases the implementation of various android UI Elements and Room database functionality.",
       img: ["/screenshots/androidToDoApp/SplashScreen.png","/screenshots/androidToDoApp/AddNewToDoItem.png","/screenshots/androidToDoApp/NewToDoListItemAdded.png"],
@@ -42,11 +42,27 @@ const PortfolioPage = () => {
     },
     {
       id: 5,
-      color: "from-purple-300 to-red-300",
+      color: "from-blue-300 to-violet-300",
       title: "Android Multi App",
       desc: "A multi-feature Android application that showcases various functionalities such as Camera, Phone and Messaging intents. This project has been extracted from a larger Android development repository for focused development, easier management, and portfolio showcasing.",
       img: ["/screenshots/androidMultiApp/splash_screen.png","/screenshots/androidMultiApp/app_selection.png","/screenshots/androidMultiApp/calling_permission.png"],
       link: "https://github.com/rmondev/Android-Multi-App",
+    },
+    {
+      id: 6,
+      color: "from-violet-300 to-red-300",
+      title: "Met Gallery Web App",
+      desc: "The Met Artwork App is an interactive web application that allows users to explore and engage with the Metropolitan Museum of Art's collection. This app supports functionality for searching, viewing detailed information, managing favorites, and tracking search history using the Museum's public API.",
+      img: ["/screenshots/webMETGalleryApp/home_screen.png"],
+      link: "https://github.com/rmondev/MET-Gallery-Web-App",
+    },
+    {
+      id: 7,
+      color: "from-red-300 to-violet-300",
+      title: "",
+      desc: "",
+      img: [],
+      link: "",
     },
    
 
@@ -81,13 +97,32 @@ const PortfolioPage = () => {
                     <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">{item.title}</h1>
                     <Link href={item.link}>
                       <div className="flex items-center justify-center gap-2">
+
+                        
+                        
                         {item.img.map((imgPath, index) => (
-                          <div key={index} className="w-24 h-52 sm:w-[98px] sm:h-[213px]       md:w-[98px] md:h-[213px] lg:w-[196px] lg:h-[426px] xl:w-[222px] xl:h-[510px] border-2 border-black md:rounded-lg lg:rounded-2xl xl:rounded-2xl rounded-md relative overflow-hidden">
-                          <Image src={imgPath} alt="" fill />
+
+                        item.img.length > 1 ? (
+                          <div key={index} 
+                          className="w-24 h-52 sm:w-[98px] sm:h-[213px] md:w-[98px] md:h-[213px] lg:w-[196px] lg:h-[426px] xl:w-[222px] xl:h-[510px] border-2 border-black md:rounded-lg lg:rounded-2xl xl:rounded-2xl rounded-md relative overflow-hidden"
+                          >
+                            <Image src={imgPath} alt="" fill />
                         </div>
+                         ) : (
+                        <div key={index} className="">
+                          <Image 
+                          className='border border-black'
+                          src={imgPath} 
+                          width={1000}
+                          height={1000}
+                          alt="" />
+                        </div>
+                         )
+                          ))}
+
+                      
 
 
-                        ))}
                       </div>
                     </Link>
                     <p className='w-80 lg:w-[500px] lg:text-lg xl:w-[600px]'>{item.desc}</p>
