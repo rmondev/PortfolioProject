63ee9643b408941b0a6f959188808a44cc1aada1
Riccardo Moncada <rmondev@gmail.com>
2024-05-02 23:31:01 -0400

    removed emoji

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -67,7 +67,7 @@ const ContactPage = () => {
       >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl overflow-hidden">
           <div>
               {text.split("").map((char, index) => (
                 <motion.span
@@ -83,8 +83,7 @@ const ContactPage = () => {
                   { char }
                 </motion.span>
 
-              ))}
-              😃
+              ))} 
             </div>
           </div>
 
