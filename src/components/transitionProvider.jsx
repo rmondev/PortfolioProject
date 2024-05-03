43904c67ebbb7266b517c35586c59e7d39f9002a
Riccardo Moncada <rmondev@gmail.com>
2024-05-03 01:31:09 -0400

    test

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -31,7 +31,7 @@ const TransitionProvider = ({children}) => {
           <div className="h-24">
             <Navbar />
           </div>
-            <div className="h-[calc(100vh-12rem)]">{children}</div>
+            <div className="h-[calc(100vh-6rem)]">{children}</div>
         </div>
       </AnimatePresence>
     </div>
