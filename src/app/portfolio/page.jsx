d556b3a5a7fca85f7ff2fbe44579035a8b0a02f0
Riccardo Moncada <rmoncada@myseneca.ca>
2025-04-20 17:14:00 -0400

    Add swipe option to slider configuration for portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -95,7 +95,8 @@ const PortfolioPage = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+    swipe: false
   };
 
   const sliderProjectShowcase = () => {
